6ac3f7150af919c259b47e3ed658287b5fccc019
Darina Kovalskaya <57908654+holtzdari@users.noreply.github.com>
2022-10-22 20:14:16 +0300

    Add create and delete task

--- server/src/data/queries.js
+++ server/src/data/queries.js
@@ -1,12 +1,1 @@
-const selectAllUsersQuery = `SELECT * FROM users`;
 
-const selectAllTasksQuery = `SELECT * FROM tasks`;
-const selectTaskByIdQuery = `SELECT * FROM tasks WHERE id = ?`;
-const deleteTaskByIdQuery = `DELETE FROM tasks WHERE id = ?`;
-
-module.exports = {
-    selectAllUsersQuery,
-    selectAllTasksQuery,
-    selectTaskByIdQuery,
-    deleteTaskByIdQuery,
-}
--- server/src/routes.js
+++ server/src/routes.js
@@ -1,11 +1,15 @@
 const { signUp } = require("./controllers/auth.controller");
-const { getTaskById, getAllTasks } = require("./controllers/task.controller");
+const { getTaskById, getAllTasks, createTask, deleteTask } = require("./controllers/task.controller");
 
 const router = (app) => {
   app.post('/auth', signUp);
 
   app.get('/task', getAllTasks);
   app.get('/task/:id', getTaskById);
+
+  app.post('/task', createTask);
+  
+  app.delete('/task/:id', deleteTask);
 };
 
 module.exports = {
